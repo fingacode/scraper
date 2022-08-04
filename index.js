19fd72d209c3718c249760a690a8dd8cf56c085e
Junior Ngoma <kidindustryuk@gmail.com>
2022-08-04 22:39:10 +0100

    Project tidy up :)

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 const puppeteer = require("puppeteer");
+const tokenCount = 1000;
+const projectUrl =
+  "https://generator.artblocks.io/0xa7d8d9ef8d8ce8992df33d8b8cf4aebabd5bd270/13000";
 
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  const projectUrl =
-    "https://generator.artblocks.io/0xa7d8d9ef8d8ce8992df33d8b8cf4aebabd5bd270/13000";
+  // Render the window at this size, this will the exported image size
+  await page.setViewport({
+    width: 1000,
+    height: 1000,
+  });
 
-  //   Optional
-    await page.setViewport({
-      width: 1000,
-      height: 1000,
-    });
+  for (let index = 0; index < tokenCount; index++) {
+    const tokenIndex = `${index}`;
+    const tokenId = tokenIndex.padStart(3, 0);
 
-  for (let index = 0; index < 1000; index++) {
-    const num = `${index}`;
-    const id = num.padStart(3, 0);
-
-    await page.goto(`${projectUrl}${id}`);
-    await page.screenshot({ path: `ringers/${id}.png` });
+    await page.goto(`${projectUrl}${tokenId}`);
+    await page.screenshot({ path: `exports/${tokenId}.png` });
   }
 
   await browser.close();
